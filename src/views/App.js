bd8843f3d750d59a8fad5626631a0cf63ae01000
Hoang Nguyen <kza10303@gmail.com>
2023-11-08 21:47:25 +0700

    create delete with react

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
+
+/**
+ *  two components: class component / function component (function, arrow)
+ */
 
 function App() {
+  // const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +15,8 @@ function App() {
         <p>
           Hello World with React.js
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <MyComponent />
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
+
+class MyComponent extends React.Component {
+
+    state = {
+        arrJobs: [
+            { id: 'h01', title: 'developer', salary: '500' },
+            { id: 'h02', title: 'tester', salary: '400' },
+            { id: 'h03', title: 'Project manager', salary: '1000' }
+        ]
+    }
+
+    addNewJob = (job) => {
+        console.log('check job from parent: ', job)
+        // let currentJobs = this.state.arrJobs;
+        // currentJobs.push(job)
+        this.setState({
+            arrJobs: [...this.state.arrJobs, job]
+            // arrJobs: currentJobs
+        })
+    }
+
+    deleteAJob = (job) => {
+        let currentJobs = this.state.arrJobs;
+        currentJobs = currentJobs.filter(item => item.id !== job.id);
+        this.setState({
+            arrJobs: currentJobs
+        })
+    }
+
+    /*
+    JSX => return block
+    fragment
+    */
+
+
+
+    render() {
+        console.log('>>> call render: ', this.state)
+        return (
+            <>
+                <AddComponent
+                    addNewJob={this.addNewJob}
+                />
+
+
+                <ChildComponent
+                    arrJobs={this.state.arrJobs}
+                    deleteAJob={this.deleteAJob}
+                />
+
+            </>
+
+        )
+    }
+}
+
+export default MyComponent;
