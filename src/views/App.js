036ac09a81ad588132e3cdebec80327ac5bbfb20
Hoang Nguyen <kza10303@gmail.com>
2023-11-09 15:16:33 +0700

    React with CRUD

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 /**
  *  two components: class component / function component (function, arrow)
@@ -13,11 +16,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World with React.js
+          Simple TODO Apps with React.js
         </p>
 
-        <MyComponent />
+        {/* <MyComponent /> */}
+        <ListTodo />
       </header>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
     </div>
   );
 }
---
+++ src/views/Todos/ListTodo.js
@@ -0,0 +1,121 @@
+import React from "react";
+import './listTodo.scss';
+import AddTodo from "./AddTodo";
+import { toast } from 'react-toastify';
+
+
+class ListTodo extends React.Component {
+
+    state = {
+        listTodos: [
+            { id: 'a01', title: 'doing homework' },
+            { id: 'a02', title: 'making video' },
+            { id: 'a03', title: 'Fixing bugs' }
+        ],
+        editTodo: {}
+    }
+
+    addNewTodo = (todo) => {
+        this.setState({
+            listTodos: [...this.state.listTodos, todo]
+        })
+
+        toast.success("Wow so easy!")
+    }
+    handleEditTodo = (todo) => {
+        let { editTodo, listTodos } = this.state;
+
+        let isEmptyObj = Object.keys(editTodo).length === 0;
+        //save
+        if (isEmptyObj === false && editTodo.id === todo.id) {
+
+            let listTodosCopy = [...listTodos]
+            let objIndex = listTodosCopy.findIndex((item => item.id === todo.id));
+
+            listTodosCopy[objIndex].title = editTodo.title;
+            this.setState({
+                listTodos: listTodosCopy,
+                editTodo: {}
+            })
+            toast.success("Update TODO succeed!")
+            return;
+        }
+        //edit
+        this.setState({
+            editTodo: todo
+        })
+    }
+    handleDeleteTodo = (todo) => {
+        let currentTodos = this.state.listTodos;
+        currentTodos = currentTodos.filter(item => item.id !== todo.id)
+        this.setState({
+            listTodos: currentTodos
+        })
+
+        toast.success("Delete succeed!")
+    }
+
+    handleOnChangeEditTodo = (event) => {
+        let editTodoCopy = { ...this.state.editTodo };
+        editTodoCopy.title = event.target.value;
+        this.setState({
+            editTodo: editTodoCopy
+        })
+    }
+
+    render() {
+        let { listTodos, editTodo } = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0;
+        console.log('>>> check empty object: ', isEmptyObj)
+
+        return (
+            <div className="list-todo-container">
+                <AddTodo
+                    addNewTodo={this.addNewTodo}
+                />
+                <div className="list-todo-content">
+                    {listTodos && listTodos.length > 0 &&
+                        listTodos.map((item, index) => {
+                            return (
+                                <div className="todo-child" key={item.id}>
+                                    {isEmptyObj === true ?
+                                        <span> {index + 1} - {item.title}</span>
+                                        :
+                                        <>
+                                            {editTodo.id === item.id ?
+                                                <span>
+                                                    {index + 1} - <input
+                                                        value={editTodo.title}
+                                                        onChange={(event) => this.handleOnChangeEditTodo(event)}
+                                                    />
+                                                </span>
+                                                :
+                                                <span>
+                                                    {index + 1} - {item.title}
+                                                </span>
+                                            }
+                                        </>
+                                    }
+                                    <button className="edit"
+                                        onClick={() => this.handleEditTodo(item)}
+                                    >
+                                        {isEmptyObj === false && editTodo.id === item.id ?
+                                            'save' : 'Edit'
+                                        }
+                                    </button>
+                                    <button className="delete"
+                                        onClick={() => this.handleDeleteTodo(item)}
+                                    >Delete</button>
+                                </div>
+                            )
+                        })
+                    }
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListTodo;
