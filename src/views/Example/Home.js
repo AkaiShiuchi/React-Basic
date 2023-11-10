248a3faf49621abc8c4efbe9d2026489c767f3b6
Hoang Nguyen <kza10303@gmail.com>
2023-11-10 10:09:12 +0700

    import images

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from '../../assets/images/react-one-1536x864.jpg';
 
 class Home extends React.Component {
 
@@ -12,9 +13,14 @@ class Home extends React.Component {
     render() {
         console.log('>>> check props: ', this.props)
         return (
-            <div>
-                Hello World from HomePage
-            </div>
+            <>
+                <div>
+                    Hello World from HomePage
+                </div>
+                <div>
+                    <img src={logo} style={{ width: '450px', height: '300px', marginTop: '20px' }} />
+                </div>
+            </>
         )
     }
 }
